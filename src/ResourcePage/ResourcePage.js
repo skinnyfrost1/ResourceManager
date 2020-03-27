162b17253853b8b8732f9293e68a0f5c9881a4e1
Xinying Shi <shi.xiny@husky.neu.edu>
2020-03-27 03:27:38 -0400

    modify-ResourcePage-table-head

--- src/ResourcePage/ResourcePage.js
+++ src/ResourcePage/ResourcePage.js
@@ -1,7 +1,7 @@
 import React,{Component, useState, useEffect} from "react";
 import "./ResourcePage.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Navbar, Form, FormControl, Button, Nav, MenuItem, Dropdown, DropdownButton} from 'react-bootstrap'
+import { Navbar, Form, FormControl, Button, Nav, MenuItem, Dropdown, DropdownButton, Table} from 'react-bootstrap'
 import DropdownItem from "react-bootstrap/DropdownItem";
 
 
@@ -9,38 +9,36 @@ import DropdownItem from "react-bootstrap/DropdownItem";
 export default function ResourcePage() {
     const [toggle, setToggle] = useState(false);
 
-    // Similar to componentDidMount and componentDidUpdate:
-    // useEffect(() => {
-        // Update the document title using the browser API
-        
-    // });
-
     return (
-        <Navbar bg="light" expand="xl" className='auto'> 
+        //search bar + user information
+        <Navbar bg="light" expand='true' className='ml-auto'> 
+
             <Nav.Item >
+                {/* e16e37 */}
                 <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button ><FontAwesomeIcon icon="search" onClick={console.log('this is search')} /></Button>
+                    <FormControl type="text" placeholder="Keyword" className="mr-sm-2" />
+                    <Button id='search'><FontAwesomeIcon icon="search" onClick={console.log('this is search')} /></Button>
                 </Form>
             </Nav.Item>
+
             <Nav.Item >
-                <span>Resource Catalog</span>
+                Resource Catalog
             </Nav.Item>
+
             <Nav.Item >
-                <Dropdown id="basic-nav-dropdown">
+                <Dropdown id="basic-nav-dropdown" >
                     <DropdownButton
-                        title={<span><FontAwesomeIcon icon="plus" /> </span>} >
-
-
-                        <Dropdown.Item href="#action/3.1"><FontAwesomeIcon icon="align-left" /> <span>Add Row</span></Dropdown.Item>
-                        <Dropdown.Item href="#action/3.2"><FontAwesomeIcon icon="columns" /> <span>Add Colunmn</span></Dropdown.Item>
-                        <Dropdown.Item href="#action/3.3"><FontAwesomeIcon icon="file-csv" /> <span>Import CSV</span></Dropdown.Item>
+                        title={<span><FontAwesomeIcon icon="plus" /> </span>} 
+                        className="navbar-toggler-right"
+                        >
+                        <Dropdown.Item className="dropdown-item-right" href="#action/3.1"><FontAwesomeIcon icon="align-left" /> <span>Add Row</span></Dropdown.Item>
+                        <Dropdown.Item className="dropdown-item-right" href="#action/3.2"><FontAwesomeIcon icon="columns" /> <span>Add Colunmn</span></Dropdown.Item>
+                        <Dropdown.Item className="dropdown-item-right" href="#action/3.3"><FontAwesomeIcon icon="file-csv" /> <span>Import CSV</span></Dropdown.Item>
                     </DropdownButton>
                 </Dropdown>
             </Nav.Item>
-                
-        
-   
-        </Navbar>
+
+         </Navbar>
+
     )
 }
