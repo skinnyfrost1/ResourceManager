cd37b8ad44d2ada2c425547b931665cecc8f71a2
Xinying Shi <shi.xiny@husky.neu.edu>
2020-03-28 00:25:09 -0400

    add csv modal

--- src/ResourcePage/ResourcePage.js
+++ src/ResourcePage/ResourcePage.js
@@ -1,7 +1,7 @@
 import React,{Component, useState, useEffect} from "react";
 import "./ResourcePage.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Navbar, Form, FormControl, Button, Nav, MenuItem, Dropdown, DropdownButton, Table} from 'react-bootstrap'
+import { Navbar, Form, FormControl, Button, Nav, MenuItem, Dropdown, DropdownButton, Modal} from 'react-bootstrap'
 import DropdownItem from "react-bootstrap/DropdownItem";
 
 
@@ -9,36 +9,65 @@ import DropdownItem from "react-bootstrap/DropdownItem";
 export default function ResourcePage() {
     const [toggle, setToggle] = useState(false);
 
+    //handle popup
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const handleImport = () => setShow(false);  //handle importing
     return (
         //search bar + user information
-        <Navbar bg="light" expand='true' className='ml-auto'> 
+        <div>
+            <Navbar bg="light" expand='true' className='ml-auto'>
 
-            <Nav.Item >
-                {/* e16e37 */}
-                <Form inline>
-                    <FormControl type="text" placeholder="Keyword" className="mr-sm-2" />
-                    <Button id='search'><FontAwesomeIcon icon="search" onClick={console.log('this is search')} /></Button>
-                </Form>
+                <Nav.Item >
+                    {/* e16e37 */}
+                    <Form inline>
+                        <FormControl type="text" placeholder="Keyword" className="mr-sm-2" />
+                        <Button id='search'><FontAwesomeIcon icon="search" onClick={console.log('this is search')} /></Button>
+                    </Form>
+                </Nav.Item>
+
+                <Nav.Item >
+                    Resource Catalog
             </Nav.Item>
 
-            <Nav.Item >
-                Resource Catalog
-            </Nav.Item>
-
-            <Nav.Item >
-                <Dropdown id="basic-nav-dropdown" >
-                    <DropdownButton
-                        title={<span><FontAwesomeIcon icon="plus" /> </span>} 
-                        className="navbar-toggler-right"
+                <Nav.Item >
+                    <Dropdown id="basic-nav-dropdown" >
+                        <DropdownButton
+                            title={<span><FontAwesomeIcon icon="plus" /> </span>}
+                            className="navbar-toggler-right"
                         >
-                        <Dropdown.Item className="dropdown-item-right" href="#action/3.1"><FontAwesomeIcon icon="align-left" /> <span>Add Row</span></Dropdown.Item>
-                        <Dropdown.Item className="dropdown-item-right" href="#action/3.2"><FontAwesomeIcon icon="columns" /> <span>Add Colunmn</span></Dropdown.Item>
-                        <Dropdown.Item className="dropdown-item-right" href="#action/3.3"><FontAwesomeIcon icon="file-csv" /> <span>Import CSV</span></Dropdown.Item>
-                    </DropdownButton>
-                </Dropdown>
-            </Nav.Item>
+                            <Dropdown.Item ><FontAwesomeIcon icon="align-left" /> <span>Add Row</span></Dropdown.Item>
+                            <Dropdown.Item ><FontAwesomeIcon icon="columns" /> <span>Add Colunmn</span></Dropdown.Item>
+                            <Dropdown.Item onClick={handleShow}><FontAwesomeIcon icon="file-csv" /> <span>Import CSV</span></Dropdown.Item>
+                        </DropdownButton>
+                    </Dropdown>
+                </Nav.Item>
 
-         </Navbar>
-
+            </Navbar>
+            {/* csv pop up */}
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Import file</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Choose File
+                    <br />
+                    <input type='file' accept=".csv"/>
+                    {/* <div>{something }</div> */}
+                </Modal.Body>
+                
+                <Modal.Footer>
+                    <Button  onClick={handleImport}>
+                        Import
+                    </Button>
+                    <Button onClick={handleClose}>
+                        Cancel
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+        
     )
 }
